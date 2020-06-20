bf1c1cf65ea7ac4af65a3dcdaaf16e54b7e908d1
Alex Bohm <bohm0080@umn.edu>
2020-06-19 21:10:08 -0500

    It works!

--- main.cpp
+++ main.cpp
@@ -50,7 +50,7 @@ std::string kernel_code =
 "    read_only image2d_t bottom,"
 "    write_only image2d_t output) {"
 "    const int2 pos = {get_global_id(0), get_global_id(1)};"
-"    float4 result = read_imagef(top,sampler, pos);"
+"    float4 result = read_imagef(top, sampler, pos) - read_imagef(bottom, sampler, pos);"
 "    write_imagef(output, pos, result);"
 "}"
 "";
